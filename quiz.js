8e873cb8d9277b0dea249f15674d5a1be92c63ba
anshu1016 <69161719+anshu1016@users.noreply.github.com>
2022-08-23 12:16:05 +0530

    Files uploaded

---
+++ quiz.js
@@ -0,0 +1,66 @@
+const quizdata = document.querySelector('.quiz-form');
+const submitBtn = document.querySelector("#submit-answer");
+const showResult = document.querySelector("#result");
+const showName = document.querySelector(".name");
+const showHighScore = document.querySelector("#score");
+const answers = ["Right","Equilateral","Scalene","Isosceles","180","360","90","90","180","180 and 270"];
+const highScores = [
+    {
+        "name":"Ankit",
+        "scored":10,
+    },{
+        "name":"Rahul",
+        "scored":9, 
+    },{
+        "name":"Shyam",
+        "scored":8,
+    }
+]
+var getName = prompt("Hi, Please enter your good name..");
+showIntro();
+function showIntro(){
+    
+    if (getName === null || !isNaN(getName)){
+        alert("Invalid name, Enter your name with only string not any numbers or special characters..");
+      }else{12342314
+        showName.innerText = "Hello "+ getName +" , Let's begin the quiz..\n Best of Luck..";
+      }
+}
+
+submitBtn.addEventListener("click",validateOptions);
+
+function validateOptions(){
+    let index = 0;
+    let score = 0;
+    let formResult = new FormData(quizdata)
+    for(let value of formResult.values() ){
+        if(value === answers[index])
+        {
+            
+            score=score+1;
+            
+        }
+        else{
+            
+            score=score-1;
+            
+        }
+        index = index+1;
+    }
+    validateScore(score);
+}
+
+function validateScore(marks){
+    if(marks >= 5 && marks<=7)
+    {
+        showResult.innerText = "Good! You scored "+marks+" \n keep Learning..";
+    }
+    else if(marks>=8 && marks<=10)
+    {
+        showResult.innerText = "Great! You scored "+marks+" \n Highest Score..";
+    }
+    else{
+        showResult.innerText = "Oh!Oh!, you scored"+ marks+ " Fail, Keep Studying";
+    }
+    
+}
